72709e43ad5db83d3b0b9ec923b04d8df2deb6c0
stefan.kendlbacher <phhjmgxtvbmays3doirhq27m7juea6rrvqfctullf4qxjkxnxrgq>
2022-05-18 23:58:10 +0200

    svelte tryout

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    let defaultLineCount = 10;
+    let divLineCount = 25;
+    let selectedLineCount;
+
+
+    $: lineCssStyles = `height: ${100 / lineCount}vh`;
+
+    $: lineCount = selectedLineCount ? selectedLineCount.id : defaultLineCount;
+    let lineCountOptions = [
+        {id: 10, text: 10},
+        {id: 15, text: 15},
+        {id: 20, text: 20},
+        {id: 50, text: 50},
+    ];
+    $: dividerCssStyles = `border-right: 1px solid red;width: ${100 / divLineCount}vw; float:left;`;
+</script>
+<style>
+    select {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+    }
+
+    :global(*) {
+        box-sizing: border-box;
+    }
+
+    :global(body) {
+        margin: 0;
+        padding: 0;
+    }
+
+    div {
+        border-bottom: 1px solid #666;
+    }
+</style>
+
+<select bind:value={selectedLineCount}>
+    {#each lineCountOptions as number}
+        <option value={number}>
+            {number.text}
+        </option>
+    {/each}
+</select>
+
+{#each {length: divLineCount} as _, i}
+<div style="{dividerCssStyles}">
+    {#if selectedLineCount}
+        {#each {length: lineCount} as _, i}
+            <div style="{lineCssStyles}"></div>
+        {/each}
+    {/if}
+</div>
+{/each}
